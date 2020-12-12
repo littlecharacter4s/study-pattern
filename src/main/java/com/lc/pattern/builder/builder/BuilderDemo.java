cad4405fdee91e322bd8338187ad3a07c92077c2
littlecharacter <littlecharacter@qq.com>
2020-12-13 00:04:03 +0800

    优化建造者模式

--- src/main/java/com/lc/pattern/builder/builder/BuilderDemo.java
+++ src/main/java/com/lc/pattern/builder/builder/BuilderDemo.java
@@ -2,7 +2,7 @@ package com.lc.pattern.builder.builder;
 
 public class BuilderDemo {
 	public static void main(String[] args) {
-	    // 正规的方式
+	    // 1.正规的方式
 		ComputerDirector cd = new ComputerDirector(new LenovoComputerBuilder());
 		Computer computer = cd.buildComputer();
 		
@@ -10,10 +10,12 @@ public class BuilderDemo {
 		System.out.println(computer.getMemory().getName());
 		System.out.println(computer.getVideo().getName());
 
-		// 简单常用的方式（通常复杂的对象属性都是不变的，如果构建过程不复杂可以省略Director）
+		// 2.简单常用的方式（通常复杂的对象属性都是不变的，如果构建过程不复杂可以省略Director）
         computer = new MacComputerBuilder().buildCpu().buildMemory().buildVideo().build();
         System.out.println(computer.getCpu().getName());
         System.out.println(computer.getMemory().getName());
         System.out.println(computer.getVideo().getName());
+
+        // 3.参见《Effective Java》第2条
 	}
 }
